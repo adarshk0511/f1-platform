3ca970cb374d8991432120fe8d1f23d91470cd8c
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-08-11 00:59:20 +0530

    Added route in gateway for POST Driver API

--- gateway/src/routes/driverRoutes.js
+++ gateway/src/routes/driverRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const {
     createProxyMiddleware,
+    fixRequestBody,
 } = require("http-proxy-middleware");
 
 const config = require("../config");
@@ -19,14 +20,21 @@ router.use(
         pathRewrite: (path) => {
 
             return `/api/v1/drivers${path}`;
+
         },
 
         on: {
 
-            proxyReq: (proxyReq, req) => {
+            proxyReq: (proxyReq, req, res) => {
 
                 console.log(
                     `[Gateway] ${req.method} ${req.originalUrl} -> Driver Service`
+                );
+
+                // Re-attach JSON body after express.json()
+                fixRequestBody(
+                    proxyReq,
+                    req
                 );
 
             },
