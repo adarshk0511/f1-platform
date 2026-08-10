86102c100d59ca3efb2f40ceabdcbbbf052f1eb4
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-08-10 23:19:49 +0530

    Made initial Gateway setup

---
+++ gateway/src/config/index.js
@@ -0,0 +1,25 @@
+const config = {
+    port: Number(process.env.PORT) || 8080,
+
+    services: {
+        driver: {
+            url:
+                process.env.DRIVER_SERVICE_URL ||
+                "http://localhost:5002",
+        },
+
+        auth: {
+            url:
+                process.env.AUTH_SERVICE_URL ||
+                "http://localhost:5001",
+        },
+
+        job: {
+            url:
+                process.env.JOB_SERVICE_URL ||
+                "http://localhost:5003",
+        },
+    },
+};
+
+module.exports = config;
